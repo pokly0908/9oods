b7d09c6085cdb3611274a1ec8512ed69d4535afa
pokly0908 <pokly0908@naver.com>
2024-03-27 21:46:27 +0900

    :tada: init project

---
+++ src/test/java/com/kuku9/goods/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuku9.goods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

5ee9be80f6ed66d75043605b0f918e9cafedac6c
OZIIJIN <dpwlsdlqma6@gmail.com>
2024-03-28 12:10:40 +0900

    :sparkles: feat(file) : File Entity 생성

--- src/main/java/com/kuku9/goods/file/entity/File.java
+++ src/main/java/com/kuku9/goods/file/entity/File.java
@@ -5,11 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class File {

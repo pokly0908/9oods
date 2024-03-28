1551949bec633ef8cd07e2caa2c97e19e9fe934b
OZIIJIN <dpwlsdlqma6@gmail.com>
2024-03-28 16:58:40 +0900

    :sparkles: feat: Q객체 추가

---
+++ src/main/generated/com/kuku9/goods/domain/event/entity/QEvent.java
@@ -0,0 +1,43 @@
+package com.kuku9.goods.domain.event.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QEvent is a Querydsl query type for Event
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QEvent extends EntityPathBase<Event> {
+
+    private static final long serialVersionUID = 1398106655L;
+
+    public static final QEvent event = new QEvent("event");
+
+    public final StringPath content = createString("content");
+
+    public final NumberPath<Long> fileId = createNumber("fileId", Long.class);
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath title = createString("title");
+
+    public QEvent(String variable) {
+        super(Event.class, forVariable(variable));
+    }
+
+    public QEvent(Path<? extends Event> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QEvent(PathMetadata metadata) {
+        super(Event.class, metadata);
+    }
+
+}
+
---
+++ src/main/generated/com/kuku9/goods/domain/user/entity/QUser.java
@@ -0,0 +1,60 @@
+package com.kuku9.goods.domain.user.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QUser is a Querydsl query type for User
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QUser extends EntityPathBase<User> {
+
+    private static final long serialVersionUID = 898266895L;
+
+    public static final QUser user = new QUser("user");
+
+    public final com.kuku9.goods.global.common.entity.QBaseEntity _super = new com.kuku9.goods.global.common.entity.QBaseEntity(this);
+
+    public final StringPath adminCode = createString("adminCode");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> deletedAt = _super.deletedAt;
+
+    public final StringPath email = createString("email");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> modifiedAt = _super.modifiedAt;
+
+    public final StringPath name = createString("name");
+
+    public final StringPath password = createString("password");
+
+    public final EnumPath<UserRoleEnum> role = createEnum("role", UserRoleEnum.class);
+
+    public final StringPath username = createString("username");
+
+    public QUser(String variable) {
+        super(User.class, forVariable(variable));
+    }
+
+    public QUser(Path<? extends User> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QUser(PathMetadata metadata) {
+        super(User.class, metadata);
+    }
+
+}
+

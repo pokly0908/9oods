5d2a11caf961bf9a2e1c46d287e25aa6f3e7f9f3
OZIIJIN <dpwlsdlqma6@gmail.com>
2024-03-28 13:59:48 +0900

    :sparkles: feat(review) : Review Entity 추가

---
+++ src/main/java/com/kuku9/goods/review/entity/Review.java
@@ -0,0 +1,32 @@
+package com.kuku9.goods.review.entity;
+
+import com.kuku9.goods.product.entity.Product;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Review {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column
+	private String content;
+
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	private Product product;
+
+}

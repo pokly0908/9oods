45f32eb4581841c6eb9b2eb11ed24d4c1bf94fa1
kiwimel0n <alstkd8580@naver.com>
2024-03-28 14:23:02 +0900

    :recycle: user 컬럼 추가

--- src/main/java/com/kuku9/goods/user/entity/User.java
+++ src/main/java/com/kuku9/goods/user/entity/User.java
@@ -4,6 +4,7 @@ import com.kuku9.goods.common.entity.BaseEntity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import org.hibernate.annotations.Comment;
 
 
 @Entity
@@ -15,19 +16,28 @@ public class User extends BaseEntity {
     private Long id;
 
     @Column(nullable = false, unique = true)
+    @Comment("사용자가 로그인할때 쓸 id")
     private String username;
 
+    @Column(nullable = false)
+    @Comment("사용자 실명")
+    private String name;
+
     @Column(nullable = false, unique = true)
+    @Comment("이메일")
     private String email;
 
     @Column(nullable = false)
+    @Comment("비밀번호")
     private String password;
 
     @Column
+    @Comment("관리자 회원가입시 사용할 코드")
     private String adminCode;
 
     @Column(nullable = false)
     @Enumerated(value = EnumType.STRING)
+    @Comment("유저 권한")
     private UserRoleEnum role = UserRoleEnum.USER;
 
 

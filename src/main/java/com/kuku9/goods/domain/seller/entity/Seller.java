6d46e73292181cb8ed4da1b44952530bf897325f
namji <skawlgns07@naver.com>
2024-03-28 12:25:46 +0900

    :sparkles: feat(seller): 셀러 엔티티 생성

---
+++ src/main/java/com/kuku9/goods/domain/seller/entity/Seller.java
@@ -0,0 +1,36 @@
+package com.kuku9.goods.domain.seller.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+@Getter
+@NoArgsConstructor
+public class Seller {
+
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  @Column
+  private String boardName;
+
+  @Column
+  private String domainNmae;
+
+  @Column
+  private String introduce;
+
+  @Column
+  private String email;
+
+  @Column
+  private String phone_number;
+
+}

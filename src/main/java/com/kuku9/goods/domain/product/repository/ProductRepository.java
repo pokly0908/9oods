a9a1cc735e5f4468ac9ec3e5bb84e485fc2ebc2b
pokly0908 <pokly0908@naver.com>
2024-03-31 15:23:10 +0900

    :recycle: fix: ProductRepository 수정

--- src/main/java/com/kuku9/goods/domain/product/repository/ProductRepository.java
+++ src/main/java/com/kuku9/goods/domain/product/repository/ProductRepository.java
@@ -1,9 +1,19 @@
 package com.kuku9.goods.domain.product.repository;
 
 import com.kuku9.goods.domain.product.entity.Product;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ProductRepository {
 
     void save(Product product);
 
+    Product findByIdAndSellerId(Long productId, Long id);
+
+    Optional<Product> findById(Long productId);
+
+    Page<Product> findAll(Pageable pageable);
+
+    Page<Product> findBySellerId(Long sellerId, Pageable pageable);
 }
--- src/main/java/com/kuku9/goods/domain/product/repository/ProductRepositoryImpl.java
+++ src/main/java/com/kuku9/goods/domain/product/repository/ProductRepositoryImpl.java
@@ -1,7 +1,10 @@
 package com.kuku9.goods.domain.product.repository;
 
 import com.kuku9.goods.domain.product.entity.Product;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -13,5 +16,25 @@ public class ProductRepositoryImpl implements ProductRepository {
     @Override
     public void save(Product product) {
         productJpaRepository.save(product);
+    }
+
+    @Override
+    public Product findByIdAndSellerId(Long productId, Long id) {
+        return productJpaRepository.findByIdAndSellerId(productId, id);
+    }
+
+    @Override
+    public Optional<Product> findById(Long productId) {
+        return productJpaRepository.findById(productId);
+    }
+
+    @Override
+    public Page<Product> findAll(Pageable pageable) {
+        return productJpaRepository.findAll(pageable);
+    }
+
+    @Override
+    public Page<Product> findBySellerId(Long sellerId, Pageable pageable) {
+        return productJpaRepository.findBySellerId(sellerId, pageable);
     }
 }

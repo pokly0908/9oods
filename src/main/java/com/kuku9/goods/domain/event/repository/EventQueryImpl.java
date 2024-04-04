9f91780b703b5aa6ea961b4b69df72b466a6954c
OZIIJIN <dpwlsdlqma6@gmail.com>
2024-04-05 01:04:17 +0900

    :recycle: refactor(event): 전체 조회 기능 N+1 문제 해결

--- src/main/java/com/kuku9/goods/domain/event/repository/EventQueryImpl.java
+++ src/main/java/com/kuku9/goods/domain/event/repository/EventQueryImpl.java
@@ -1,5 +1,6 @@
 package com.kuku9.goods.domain.event.repository;
 
+import com.kuku9.goods.domain.event_product.entity.EventProduct;
 import com.kuku9.goods.domain.event_product.entity.QEventProduct;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
@@ -12,11 +13,11 @@ public class EventQueryImpl implements EventQuery {
 
     private final JPAQueryFactory jpaQueryFactory;
 
-    public List<Long> getEventProducts(Long eventId) {
+    public List<EventProduct> getEventProducts(List<Long> eventIds) {
         return jpaQueryFactory.select(
-                QEventProduct.eventProduct.product.id)
+                QEventProduct.eventProduct)
             .from(QEventProduct.eventProduct)
-            .where(QEventProduct.eventProduct.event.id.eq(eventId))
+            .where(QEventProduct.eventProduct.event.id.in(eventIds))
             .fetch();
     }
 

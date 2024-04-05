909de4856a868f0d280daf0f37c8eabcf9de04e3
kiwimel0n <alstkd8580@naver.com>
2024-04-05 11:30:49 +0900

    :recycle: cuponTest 주석화

--- src/test/java/com/kuku9/goods/coupon/CouponTest.java
+++ src/test/java/com/kuku9/goods/coupon/CouponTest.java
@@ -12,27 +12,27 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-public class CouponTest {
-
-	@Autowired
-	private EventServiceImpl eventService;
-
-	@Autowired
-	private CouponRepository couponRepository;
-
-
-	@Test
-	@DisplayName("50 수량의 선착순 쿠폰에 50명의 유저가 접근하는 상황 가정")
-	void test() {
-
-		IntStream.range(0, 50).parallel()
-			.forEach(i -> {
-				Coupon coupon = couponRepository.findById(4L).orElseThrow();
-				coupon.decrease();
-				couponRepository.save(coupon);
-				System.out.println("남은 쿠폰의 수량: " + coupon.getQuantity());
-			});
-	}
-
-}
+//@SpringBootTest
+//public class CouponTest {
+//
+//	@Autowired
+//	private EventServiceImpl eventService;
+//
+//	@Autowired
+//	private CouponRepository couponRepository;
+//
+//
+//	@Test
+//	@DisplayName("50 수량의 선착순 쿠폰에 50명의 유저가 접근하는 상황 가정")
+//	void test() {
+//
+//		IntStream.range(0, 50).parallel()
+//			.forEach(i -> {
+//				Coupon coupon = couponRepository.findById(4L).orElseThrow();
+//				coupon.decrease();
+//				couponRepository.save(coupon);
+//				System.out.println("남은 쿠폰의 수량: " + coupon.getQuantity());
+//			});
+//	}
+//
+//}

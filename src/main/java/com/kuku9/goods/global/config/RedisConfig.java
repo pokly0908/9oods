97e66baf7ae258c921fd4a36e5145bc26b0e0ed2
OZIIJIN <dpwlsdlqma6@gmail.com>
2024-04-08 18:24:41 +0900

    :recycle: refactor: redis config 파일 통합

--- src/main/java/com/kuku9/goods/global/config/RedisConfig.java
+++ src/main/java/com/kuku9/goods/global/config/RedisConfig.java
@@ -1,5 +1,8 @@
 package com.kuku9.goods.global.config;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +16,8 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
+    private static final String REDISSON_HOST_PREFIX = "redis://";
 
     @Value("${spring.data.redis.host}")
     private String redisHost;
@@ -36,5 +41,14 @@ public class RedisConfig {
         redisTemplate.setHashValueSerializer(new StringRedisSerializer());
         redisTemplate.setConnectionFactory(redisConnectionFactory());
         return redisTemplate;
+    }
+
+    @Bean
+    public RedissonClient redissonClient(){
+        Config config = new Config();
+        config.useSingleServer()
+            .setAddress(REDISSON_HOST_PREFIX + redisHost + ":" + redisPort);
+
+        return Redisson.create(config);
     }
 }

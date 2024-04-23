2d7de1823734351c2469cfe6745de4cabf8c5630
kiwimel0n <129644629+kiwimel0n@users.noreply.github.com>
2024-04-23 11:29:50 +0900

    Feat : 카카오 소셜 로그인 기능 추가 (#62)
    * :refactor: authController import 최적화 및
    webConfig cors SecurityConfig 통합
    
    * :refactor: username 명확성을 위한 필드 명 변경 및 username 관련 email 변경 회원가입 유형 필드 추가
    
    * :refactor: 유닛테스트 실패 유저 필드 추가에 따른 유저 회원가입 유형 추가
    
    * :refactor: 카카오로그인을 위한 환경변수 추가
    
    * :sparkles: 홈페이지 조회 컨트롤러 추가
    
    * :sparkles: 카카오 로그인 api 구현
    
    * :recycle: 유저 객체 kakaoId 필드 추가에 따른 테스트 코드수정
    
    * :recycle: 페이지 조회 api에 따른 보안설정 수정
    
    * :recycle: 카카오 정보 properties화
    
    * :recycle: 카카오 정보 properties화
    
    * :sparkles: 카카오 소셜로그인 구현
    
    * :memo: dev 환경 카카오 redirecturl 수정
    
    * :recycle: application.yml 최신화

--- src/main/java/com/kuku9/goods/domain/user/repository/UserRepository.java
+++ src/main/java/com/kuku9/goods/domain/user/repository/UserRepository.java
@@ -9,4 +9,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
     Optional<User> findByEmail(String username);
+
+    Optional<User> findByKakaoId(Long kakaoId);
 }
---
+++ src/main/java/com/kuku9/goods/global/config/RestTemplateConfig.java
@@ -0,0 +1,22 @@
+package com.kuku9.goods.global.config;
+
+import java.time.Duration;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder
+            .setConnectTimeout(Duration.ofSeconds(10))
+            .setReadTimeout(Duration.ofSeconds(10))
+            .build();
+
+    }
+}

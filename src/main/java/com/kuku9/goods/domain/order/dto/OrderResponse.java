9fe3a74cafaf2042c48b96593a18e5135bc937de
kiwimel0n <alstkd8580@naver.com>
2024-04-24 12:51:06 +0900

    :recycle: 주문 Response 주문일자 포맷변경

--- src/main/java/com/kuku9/goods/domain/order/dto/OrderResponse.java
+++ src/main/java/com/kuku9/goods/domain/order/dto/OrderResponse.java
@@ -4,25 +4,31 @@ import com.kuku9.goods.domain.order.entity.Order;
 import com.kuku9.goods.domain.order.entity.OrderStatus;
 import com.kuku9.goods.domain.product.dto.ProductResponse;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.List;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Value;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Getter
 @Value
+@NoArgsConstructor(force = true)
 public class OrderResponse {
 
     Long orderId;
     OrderStatus orderStatus;
-    LocalDateTime orderDate;
+    String orderDate;
     String address;
     List<ProductResponse> products;
 
     public OrderResponse(Order order, List<ProductResponse> products) {
         this.orderId = order.getId();
         this.orderStatus = order.getStatus();
-        this.orderDate = order.getCreatedAt();
+        this.orderDate = order.getCreatedAt().format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.FULL));
         this.address = order.getAddress();
         this.products = products;
     }
+
 }
